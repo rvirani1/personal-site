c7aad4bbe68b30693211fe2a5dbffdb97d4c3030
Riaz Virani <riaz.n.virani@gmail.com>
2021-02-08 17:44:50 -0500

    Add framer motion transitions

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import { Header } from './layout/Header'
 import styles from './Layout.module.css'
 
@@ -6,7 +7,14 @@ export function Layout({ children }) {
   return (
     <div className={styles.container}>
       <Header />
-      {children}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.25 }}
+      >
+        {children}
+      </motion.div>
     </div>
   )
 }
