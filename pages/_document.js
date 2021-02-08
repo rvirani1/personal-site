aca70a0f5254ee019df989b8becc370cf515b482
Riaz Virani <riaz.n.virani@gmail.com>
2021-02-08 11:48:59 -0500

    Change logo color, add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,16 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" href="/favicon.ico" />
+
+          {/* Start Favicon stuff */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="theme-color" content="#222222" />
+          {/* End Favicon stuff */}
         </Head>
         <body>
           <Main />
