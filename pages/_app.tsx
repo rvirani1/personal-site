7203ecd8c5f3d809fecfb241341b45d7f1dfce0a
Riaz Virani <riaz.n.virani@gmail.com>
2021-02-24 21:23:09 -0500

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
 import '../styles/globals.scss'
 
+import { pageView } from '../lib/gtag'
+
 function MyApp({ Component, pageProps, router }: any) {
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageView(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <Component {...pageProps} key={router.route} /> // eslint-disable-line react/jsx-props-no-spreading
   )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,8 @@ import Document, {
   Html, Head, Main, NextScript,
 } from 'next/document'
 
+import { GA_TRACKING_ID } from '../lib/gtag'
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -22,6 +24,25 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#2b5797" />
           <meta name="theme-color" content="#222222" />
           {/* End Favicon stuff */}
+
+          {/* Start Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          {/* End Global Site Tag (gtag.js) - Google Analytics */}
         </Head>
         <body>
           <Main />
